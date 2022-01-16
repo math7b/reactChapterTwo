539c9ae639607dd6709e07e87e81a46e0223a712
Hiragy7 <hiragi.barth@gmail.com>
2022-01-16 18:39:53 -0300

    Developed the components

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -4,11 +4,12 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --background: #f0f2f5;
         --red: #e52e4d;
+        --green: #33cc95;
         --blue: #5429cc;
         --blue-light: #6933ff;
         --text-title: #363f5f;
         --text-body: #969cb3;
-        --shap: #ffffff;
+        --shape: #ffffff;
     }
 
     * {

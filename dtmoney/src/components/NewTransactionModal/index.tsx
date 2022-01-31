5c5608d0effa6590283a0dee944d8de88a867152
Hiragy7 <hiragi.barth@gmail.com>
2022-01-31 10:11:00 -0300

    a hook

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,20 +1,19 @@
 import { FormEvent, useContext, useState } from 'react'
 import Modal from 'react-modal'
-import { api } from '../../services/api'
-import { TransactionsContext } from '../../TransactionsContext'
 
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
+import { useTransactions } from '../../hooks/useTransactions'
 interface NewTransactionModalProps {
     isOpen: boolean;
     onRequestClose: () => void;
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-    const { createTransaction } = useContext(TransactionsContext);
+    const { createTransaction } = useTransactions()
 
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState(0);
--- dtmoney/src/hooks/useTransactions.tsx
+++ dtmoney/src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from 'react';
-import { api } from './services/api';
+import { createContext, useEffect, useState, ReactNode, useContext } from 'react';
+import { api } from '../services/api';
 
 interface Transaction {
     id: number;
@@ -21,9 +21,15 @@ interface TransactionsContextData {
     createTransaction: (Transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
     {} as TransactionsContextData
 );
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext);
+
+    return context;
+}
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([])
